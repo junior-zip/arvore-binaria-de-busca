f13e60b92616efec3e0dd2fd4232bbe7c1a0e5b2
junior-zip <junior26bmx@gmail.com>
2023-09-30 19:38:59 -0300

    add removendo e inserindo por recursividade

--- src/ArvoreBinariaBusca.java
+++ src/ArvoreBinariaBusca.java
@@ -6,229 +6,121 @@ public class ArvoreBinariaBusca<T extends Comparable<T>> implements IArvoreBinar
         this.raiz = null;
     }
 
+    public void inserir(No no, Object valor) throws Exception {
+        if (valor == null) {
+            throw new IllegalArgumentException("Valor não pode ser nulo.");
+        }
+
+        if (raiz == null) {
+            raiz = new No((Comparable) valor);
+        } else {
+            inserirRecursivo(raiz, valor);
+        }
+    }
+
+
+    public void inserirRecursivo(No atual, T valor) throws Exception {
+        if (valor.compareTo((T) atual.getValor()) < 0) {
+            if (atual.getFilhoEsquerdo() == null) {
+                atual.setFilhoEsquerdo(new No(valor));
+            } else {
+                inserirRecursivo(atual.getFilhoEsquerdo(), valor);
+            }
+        } else {
+            if (atual.getFilhoDireito() == null) {
+                atual.setFilhoDireito(new No(valor));
+            } else {
+                inserirRecursivo(atual.getFilhoDireito(), valor);
+            }
+        }
+    }
+
+
     @Override
     public void inserir(Object valor) throws Exception {
-        System.out.println(valor);
+
     }
 
     @Override
     public void inserirRecursivo(No no, Object valor) {
-        if(raiz == null){
-            raiz = no;
-        }else{
-            if(no.valor.compareTo(raiz.getValor()) < 0){
-                if(raiz.getFilhoEsquerdo() == null){
-                    raiz.setFilhoEsquerdo(no);
-                }else{
-                    inserirRecursivo(no, raiz.getFilhoEsquerdo());
-                }
-            }else{
-                if (raiz.getFilhoDireito() == null){
-                     raiz.setFilhoDireito(no);
-                }else{
-                    inserirRecursivo(no, raiz.getFilhoDireito());
-                }
-            }
-        }
+
     }
 
-
-    // inacabado
-    @Override
-    public No removerRecursivo(No no, Object valor) throws Exception {
-        if(no.valor == valor){
-            no = null;
-
-        }else if(no.valor.compareTo(valor) > 0){
-
-            if(no.filhoEsquerdo.valor.equals(valor)){
-
-            }else{
-                removerRecursivo(no.filhoEsquerdo, valor);
-            }
-        }else{
-            if(no.filhoDireito.valor.equals(valor)){
-                removerNoDireita(no);
-
-            }else{
-                removerRecursivo(no.filhoDireito, valor);
-            }
+    public No remover(Object valor) throws Exception {
+        if (raiz == null) {
+            throw new Exception("Árvore vazia. Não é possível remover.");
         }
-        return null;
+
+        raiz = removerRecursivo(raiz, valor);
+        return raiz;
     }
 
-    @Override
-    public No remover(Object v) throws Exception{
-
-        if(raiz == null){
-            throw new Exception("Sem valor para acessar!");
+    public No removerRecursivo(No<T> atual, T valor) throws Exception {
+        if (atual == null) {
+            throw new Exception("Expressão incapaz de ser realizada");
         }
-        No atual = raiz;
-        No pai = raiz;
-        boolean filho_esq = true;
 
-        while(atual.getValor() != v) {
-            pai = atual;
-
-            if (atual.getValor().compareTo(v) < 0) {
-
-                atual = atual.getFilhoEsquerdo();
-                filho_esq = true;
-
-            } else {
-                atual = atual.getFilhoDireito();
-                filho_esq = false;
+        if (valor.compareTo(atual.getValor()) < 0) {
+            atual.setFilhoEsquerdo(removerRecursivo(atual.getFilhoEsquerdo(), valor));
+        } else if (valor.compareTo(atual.getValor()) > 0) {
+            atual.setFilhoDireito(removerRecursivo(atual.getFilhoDireito(), valor));
+        } else {
+            if (atual.getFilhoEsquerdo() == null) {
+                return atual.getFilhoDireito();
+            } else if (atual.getFilhoDireito() == null) {
+                return atual.getFilhoEsquerdo();
             }
-            if (atual == null) {
-                return null;
-            }
+
+            atual.setValor((T) encontrarMenorValor(atual.getFilhoDireito()));
+            atual.setFilhoDireito(removerRecursivo(atual.getFilhoDireito(), atual.getValor()));
         }
-        if(atual.filhoEsquerdo == null && atual.filhoDireito == null){
-            if(atual == raiz){
-                raiz = null;
 
-            }else if(filho_esq){
-                pai.setFilhoEsquerdo(null);
-            }else{
-                pai.setFilhoDireito(null);
-            }
-        }else if(atual.getFilhoDireito() == null){
-            if(atual == raiz){
-                raiz = atual.getFilhoEsquerdo();
-
-            }else if(filho_esq){
-                pai.setFilhoEsquerdo(atual.getFilhoEsquerdo());
-
-            }else{
-                pai.setFilhoDireito(atual.getFilhoEsquerdo());
-            }
-
-        }else if(atual.getFilhoEsquerdo() == null){
-            if(atual == raiz){
-                raiz = atual.getFilhoDireito();
-
-            }else if(filho_esq){
-                pai.setFilhoEsquerdo( atual.getFilhoDireito());
-
-            }else{
-                pai.setFilhoDireito(atual.getFilhoDireito());
-            }
-        }else{
-            No sucessor = no_sucessor(atual);
-
-            if(atual == raiz){
-                raiz = sucessor;
-            }else if(filho_esq){
-                pai.setFilhoEsquerdo(sucessor);
-            }else{
-                pai.setFilhoDireito(sucessor);
-            }
-            sucessor.setFilhoEsquerdo( atual.getFilhoEsquerdo());
-        }
         return atual;
     }
 
-    public No no_sucessor(No apaga){
-
-        No paiDoSucessor = apaga;
-        No sucessor = apaga;
-        No atual = apaga.getFilhoDireito();
-        while(atual != null){
-
-            paiDoSucessor = sucessor;
-            sucessor = atual;
+    private T encontrarMenorValor(No<T> atual) {
+        T menorValor = atual.getValor();
+        while (atual.getFilhoEsquerdo() != null) {
+            menorValor = (T) atual.getFilhoEsquerdo().getValor();
             atual = atual.getFilhoEsquerdo();
         }
-        if(sucessor != apaga.getFilhoDireito()){
-            paiDoSucessor.setFilhoEsquerdo(sucessor.getFilhoDireito());
-            sucessor.setFilhoDireito(apaga.getFilhoDireito());
-        }
-        return sucessor;
+        return menorValor;
     }
-
-    
-
 
 
     @Override
-    public No buscar(No no) throws Exception {
+    public No removerRecursivo(No no, Object valor) throws Exception {
         return null;
     }
-    //--------------------------------------
 
-    public boolean buscaNo(No<T> arvore, T valor){
-        boolean ret = false;
 
-        if(arvore == null){
-            ret = false;
+    //---------------------------------
 
-        }else if(arvore.valor == valor){
-            ret = true;
+
+    public No buscar(No no, Object valor) throws Exception{
+        No<T> ret;
+
+        if(no == null){
+           throw new Exception("Operação incapaz de ser realizada!");
+
+        }else if(no.getValor() == valor){
+            ret = (No<T>) no.getValor();
 
         }else{
-            if(arvore.valor.compareTo(valor) > 0){
-                ret =  buscaNo(arvore.filhoEsquerdo, valor);
+            if(no.getValor().compareTo(valor) > 0){
+                ret =  buscar(no.getFilhoEsquerdo(), valor);
 
             }else{
-                ret = buscaNo(arvore.filhoDireito, valor);
+                ret = buscar(no.getFilhoDireito(), valor);
             }
         }
         return ret;
     }
 
-    // -----
-    //Auxiliar
-    public int totalFilhos(No<T> no){
-        // 0 sem filhos | 1 só filho esquerda | 2 só filho direita | 3 filhos nos dois
-        int total = 0;
 
-        if(no.filhoEsquerdo != null){
-            total += 1;
-        }
-        if(no.filhoDireito != null){
-            total += 2;
 
-        }
-        return total;
-    }
-    //----------------
 
-    public void removerSemFilhos(No<T> no, T subArvore){
-        if(subArvore.equals("E")){
-            no.filhoEsquerdo = null;
 
-        }else{
-            no.filhoDireito = null;
-
-        }
-    }
-    //----------
-    //inacabado
-    public void removerNoDireita(No<T> noPai){
-        int filhos = 0;
-        No<T> noRemover = null;
-        noRemover = noPai.filhoDireito;
-        filhos = totalFilhos(noRemover);
-        if(filhos == 0){
-            removerSemFilhos(noPai, (T) "D");
-        }else if(filhos == 1){
-            removerFilhosDireita(noPai, "E");
-
-        }else if(filhos ==1 ){
-            removerFilhosDireita(noPai, "D");
-        }else if(filhos == 3){
-            //removerFilhosEsqDir();
-        }
-    }
-
-    //-------------
-    public void removerFilhosDireita(No<T> noPai, String subarvore){
-        if(subarvore.equals("E")){
-            noPai.filhoDireito = noPai.filhoDireito.filhoEsquerdo;
-        }else{
-            noPai.filhoDireito = noPai.filhoDireito.filhoDireito;
-        }
-    }
 
     @Override
     public No visitar(No no) throws Exception {
--- src/IArvore.java
+++ src/IArvore.java
@@ -2,10 +2,9 @@ public interface IArvore<T> {
 
     void inserir(T valor) throws Exception;
     void inserirRecursivo(No no, T valor);
-    No remover(Object v) throws Exception;
-    No no_sucessor(No apaga);
+    No remover(Object valor) throws Exception;
     No removerRecursivo(No no, T valor) throws Exception;
-    No buscar(No no) throws Exception;
+    No buscar(No no, T valor) throws Exception;
     No visitar(No no) throws Exception;
     boolean estaVazia();
     boolean estaCompleta();
